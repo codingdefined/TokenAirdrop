3f1c748f61ef4969ba5fcc0815f4d8af9bff6fb8
Gopesh Sharma <codingdefined@gmail.com>
2019-10-04 10:22:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,9 @@ var config = {
     accountName : "", //Please insert the sending account name here.
     accountPrivateActiveKey : "",
     tokenSymbol : "RHB", //token name
-    mode : "issue" //issue or transfer
+    mode : "issue", //issue or transfer,
+    totalToken: 3000,
+    account: 'reality.curate'
 }
 
 module.exports = {
